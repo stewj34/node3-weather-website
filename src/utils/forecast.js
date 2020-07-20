18328e8edb0181a7af69606e84c4d6fee54b1847
stewj34 <myryzen@stewjmcu.com>
2020-07-20 13:46:52 -0500

    Add text to About page, & humidity to forcast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, 'It is: '+ body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, 'It is: '+ body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain. The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
